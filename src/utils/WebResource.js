eda159c92489f5bbd24d74331a426d9314d477df
latin-programmer <latin-programmer@users.noreply.github.com>
2019-06-01 13:39:58 -0500

    Changed to one-liner

--- src/utils/WebResource.js
+++ src/utils/WebResource.js
@@ -3,22 +3,7 @@ window.tacticsUtils = window.tacticsUtils || {};
 (function () {
     'use strict';
 
-    tacticsUtils.getWebResourceJSON = function (url){ 
-        return new Promise(function(resolve, reject) {
-           fetch(url).then(function(response) {
-               if (response.ok)
-                {
-                    resolve(response.json());
-                }
-                else
-                {
-                    reject(Error('Error when getting web resource.'));
-                }
-           }
-           ).catch(function(error) 
-           {
-            reject(Error(error));
-           });
-        });
-    };
+    tacticsUtils.getWebResourceJSON = function (url) {
+        return fetch(url).then(response => response.json());
+    }
 })();  
