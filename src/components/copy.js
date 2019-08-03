e07ef57d81c00b9f0fa90d418d3aa12694873776
Dave Clark <tactics@dollardns.net>
2019-08-03 15:16:58 -0400

    Fixed copying links on iPhone

--- src/components/copy.js
+++ src/components/copy.js
@@ -2,12 +2,18 @@ export default text => {
   let input = document.createElement('INPUT');
   input.type = 'text';
   input.value = text;
-  input.setAttribute('readonly', '');
+  input.readOnly = true;
   input.style.position = 'absolute';
+  input.style.top = window.pageYOffset+'px';
   input.style.left = '-9999px';
+  input.style.fontSize = '12pt';
+  input.style.border = '0';
+  input.style.padding = '0';
+  input.style.margin = '0';
   document.body.appendChild(input);
 
   input.select();
+  input.setSelectionRange(0, input.value.length);
   document.execCommand('copy', false);
   input.remove();
 };
