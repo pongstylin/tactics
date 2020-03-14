30a20f67ec0ed115c33b964da229069beca82703
Dave Clark <tactics@dollardns.net>
2020-03-14 08:47:03 -0400

    Detect and warn about Firefox misconfiguration

--- src/install.js
+++ src/install.js
@@ -1,9 +1,34 @@
 import { getWorkerVersion } from 'client/Update.js';
+import whenDOMReady from 'components/whenDOMReady.js';
+import popup from 'components/popup.js';
 
 if ('serviceWorker' in navigator) {
   let sw = navigator.serviceWorker;
   if (!sw.controller)
-    sw.register('/sw.js');
+    sw.register('/sw.js').catch(async error => {
+      if (
+        navigator.userAgent.indexOf('Firefox') > -1 &&
+        error.message === 'The operation is insecure.'
+      ) {
+        let isWarned = sessionStorage.getItem('isWarnedInsecure');
+        if (!isWarned) {
+          await whenDOMReady;
+          popup({
+            title: 'Warning',
+            message: `
+              Firefox is configured to clear cookies when it is closed.
+              This means your account will be lost every time you close Firefox.
+              So you will not be able to complete games-in-progress if you close Firefox.
+              Consider disabling this setting.
+            `,
+            minWidth: '300px',
+          });
+          sessionStorage.setItem('isWarnedInsecure', true);
+        }
+      }
+      else
+        throw error;
+    });
 
   if (ENVIRONMENT !== 'production') {
     sw.getRegistration().then(async reg => {
