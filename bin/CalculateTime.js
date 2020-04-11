885590effb7d16e83f461c6f4e353cfddcd59c99
Matthew Roe <mattwilroe@gmail.com>
2020-04-11 10:30:33 -0500

    Migrate CalculateTime to bin folder.  Account for adding time to who evers turn it currently is.

--- bin/CalculateTime.js
+++ bin/CalculateTime.js
@@ -3,22 +3,25 @@ import fs from 'fs'
 
 var gameArg = process.argv[2];
 
-
 calculateWinner(gameArg);
 
+//Calculates the winner given a game Id
 function calculateWinner(gameId){
     let contents = loadGameFileContents(gameId);
     let [player1Time, player2Time] = calculateMoveTime(contents);
     let teams = contents.state.teams;
     let player1 = teams.filter(team => team.id == 0);
     let player2 = teams.filter(team => team.id == 1);
+    console.log( player1[0].name + " took " + player1Time);
+    console.log( player2[0].name + " took " + player2Time);
     if (player1Time > player2Time){
         console.log("Winner is " + player2[0].name);
         console.log("Player ID is " + player2[0].playerId)
+
     }
-    else if (player2Time > player2Time){
+    else if (player2Time > player1Time){
         console.log("Winner is " + player1[0].name);
-        console.log("Player ID is " + player2[0].playerId)
+        console.log("Player ID is " + player1[0].playerId)
     }
     else {
         console.log("Wooooooow it's a draw?!?!?!")
@@ -27,24 +30,26 @@ function calculateWinner(gameId){
 
 }
 
+//Reads and returns Game file
 function loadGameFileContents(gameId){
 
-    let rawContents = fs.readFileSync('../src/data/files/game_'+ gameId +'.json');
+    let rawContents = fs.readFileSync('./src/data/files/game_'+ gameId +'.json');
     let gameContents = JSON.parse(rawContents);
     return gameContents;
 
 }
 
+//Returns an array of the total time it took two teams to make a move [Team1Time, Team2Time]
 function calculateMoveTime(contents){
 
     let turns = contents.state.turns;
-    let [ player1Time , player2Time ] = turns.map(function(turn, index) {
+    let playerTimes = turns.map(function(turn, index) {
         if (turns.length > index + 1){
            let nextTurnTime = turns[ index + 1 ].started;
            return new Date(nextTurnTime) - new Date(turn.started)
         }
         else { return new Date(contents.state.turnStarted) - new Date(turn.started)}
-        
+    //Reduce to an array of values lets us only iterate over the map once.  Sacrifices readibility for speed.    
     }).reduce(function(acc, val, index){
         if (index % 2 == 0) {
             if (index == 0){
@@ -60,9 +65,10 @@ function calculateMoveTime(contents){
         }
         return acc;
     },[]);
-    //let player2Turns = turns.filter((turn, index) => index % 2 != 0);
-    return [player1Time,player2Time];
 
+    //Need to add the time between last turn ending and current turn, can't be done in the iterators above
+    let lastTurn = (turns.length) % 2; //length - 1 would tell us who made the last turn, turns.length gives us the next turn
+    playerTimes[lastTurn] += new Date() - new Date(contents.state.turnStarted);
+    return playerTimes;
 
 }
-//790aa290-0840-41c3-b5d4-f7e631e0c91c
