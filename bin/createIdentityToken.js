ba443ff326219414d1a3dedced1ea6a59610a0a0
Dave Clark <tactics@dollardns.net>
2020-05-11 14:09:18 +0000

    Rework account recovery and testing strategy
    An identity token can be generated for a player's account and used
    to create a new device.  This is a manual way to restore a player's
    account if they lose it.  Or, it can be used to gain access to a
    player's account to test bugs.  In the latter case and if there is
    an existing unused identity token, be sure to restore it after use.

---
+++ bin/createIdentityToken.js
@@ -0,0 +1,40 @@
+/*
+ * This is useful for testing or recovering a player's account.
+ */
+import dataFactory from 'data/adapterFactory.js';
+
+const dataAdapter = dataFactory();
+
+let playerId = process.argv[2];
+let tokenValue = process.argv[3];
+
+(async () => {
+  let player = await dataAdapter.getPlayer(playerId);
+
+  if (tokenValue) {
+    if (player.identityToken) {
+      console.log('There is a conflict with an existing token');
+      process.exit(1);
+    }
+
+    player.identityToken = tokenValue;
+
+    await dataAdapter.savePlayer(player);
+
+    console.log('Token restored');
+  }
+  else {
+    if (player.identityToken) {
+      console.log('Using an existing identity token.');
+      console.log(`  To restore: npm run script bin/createIdentityToken.js ${playerId} ${player.identityToken}`);
+      console.log('');
+
+      tokenValue = player.identityToken;
+    }
+    else {
+      tokenValue = await dataAdapter.createIdentityToken(playerId);
+    }
+
+    console.log(`/addDevice.html?${tokenValue}`);
+  }
+})();
