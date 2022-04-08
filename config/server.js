dafdc01793ee2640ef2f5829f4b7306d49a7908d
Aaron Snobel <aaronsnobel@aaronsnobel.ca>
2022-04-08 14:49:00 -0400

    redis initial commit

--- config/server.js
+++ config/server.js
@@ -1,5 +1,5 @@
-import pkg from '../package.json';
-import gameTypes from 'data/files/game/game_types.json';
+import pkg from '../package.json' assert { type: 'json' };
+import gameTypes from 'data/files/game/game_types.json' assert { type: 'json' };
 
 const gameTypeIds = gameTypes.map(gt => gt[0]);
 
---
+++ src/data/DataAdapter/AuthAdapter.js
@@ -0,0 +1,119 @@
+import fs from 'fs';
+
+import serializer from 'utils/serializer.js';
+import FileAdapter from 'data/FileAdapter.js';
+import migrate, { getLatestVersionNumber } from 'data/migrate.js';
+import Player from 'models/Player.js';
+
+export default class extends FileAdapter {
+  constructor() {
+    super({
+      name: 'auth',
+      fileTypes: new Map([
+        [
+          'player', {
+            saver: '_savePlayer',
+          },
+        ],
+      ]),
+    });
+  }
+
+  /*****************************************************************************
+   * Public Interface
+   ****************************************************************************/
+  async createPlayer(player) {
+    await this._createPlayer(player);
+    this.cache.get('player').add(player.id, player);
+  }
+  async openNewPlayer(player) {
+    await this._createPlayer(player);
+    this.cache.get('player').open(player.id, player);
+  }
+  async openPlayer(playerId) {
+    const player = await this._getPlayer(playerId);
+    return this.cache.get('player').open(playerId, player);
+  }
+  closePlayer(playerId) {
+    return this.cache.get('player').close(playerId);
+  }
+  async getPlayer(playerId) {
+    const player = await this._getPlayer(playerId);
+    return this.cache.get('player').add(playerId, player);
+  }
+  getOpenPlayer(playerId) {
+    return this.cache.get('player').getOpen(playerId);
+  }
+
+  /*****************************************************************************
+   * Private Interface
+   ****************************************************************************/
+  async _createPlayer(player) {
+    const buffer = this.buffer.get('player');
+
+    await this.createFile(`player_${player.id}`, () => {
+      const data = serializer.transform(player);
+      data.version = getLatestVersionNumber('player');
+
+      player.once('change', () => buffer.add(player.id, player));
+      return data;
+    });
+  }
+  async _getPlayer(playerId) {
+    const cache = this.cache.get('player');
+    const buffer = this.buffer.get('player');
+
+    if (cache.has(playerId))
+      return cache.get(playerId);
+    else if (buffer.has(playerId))
+      return buffer.get(playerId);
+
+    return this.getFile(`player_${playerId}`, data => {
+      const player = serializer.normalize(migrate('player', data));
+
+      player.once('change', () => buffer.add(playerId, player));
+      return player;
+    });
+  }
+  async _savePlayer(player) {
+    const buffer = this.buffer.get('player');
+
+    await this.putFile(`player_${player.id}`, () => {
+      const data = serializer.transform(player);
+      data.version = getLatestVersionNumber('player');
+
+      player.once('change', () => buffer.add(player.id, player));
+      return data;
+    });
+  }
+
+  /*
+   * Only used for testing right now.
+   */
+  async deletePlayer(playerId) {
+    await this.deleteFile(`player_${playerId}`);
+    //await this.deleteFile(`player_${playerId}_sets`);
+    //await this.deleteFile(`player_${playerId}_games`);
+  }
+
+  /*
+   * Not intended for use by applications.
+   */
+  listAllPlayerIds() {
+    return new Promise((resolve, reject) => {
+      const playerIds = [];
+      const regex = /^player_(.{8}-.{4}-.{4}-.{4}-.{12})\.json$/;
+
+      fs.readdir(this.filesDir, (err, fileNames) => {
+        for (let i=0; i<fileNames.length; i++) {
+          const match = regex.exec(fileNames[i]);
+          if (!match) continue;
+
+          playerIds.push(match[1]);
+        }
+
+        resolve(playerIds);
+      });
+    });
+  }
+};
