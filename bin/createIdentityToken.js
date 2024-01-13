515f6f3172f63f5faa34e4c89e4518d009b2ea90
Dave Clark <tactics@dollardns.net>
2024-01-13 12:53:07 -0500

    Compatibility update for createIdentityToken.js

--- bin/createIdentityToken.js
+++ bin/createIdentityToken.js
@@ -1,42 +1,33 @@
 /*
  * This is useful for testing or recovering a player's account.
  */
-import 'plugins/array.js';
-import 'plugins/set.js';
-import 'plugins/map.js';
-import 'plugins/string.js';
-import AuthAdapter from 'data/FileAdapter/AuthAdapter.js';
+import '#plugins/index.js';
+import AuthAdapter from '#data/FileAdapter/AuthAdapter.js';
 
-const dataAdapter = new AuthAdapter();
+const playerId = process.argv[2];
+const tokenValue = process.argv[3];
+const dataAdapter = await new AuthAdapter().bootstrap();
+const player = await dataAdapter.getPlayer(playerId);
 
-let playerId = process.argv[2];
-let tokenValue = process.argv[3];
-
-(async () => {
-  let player = await dataAdapter.getPlayer(playerId);
-
-  if (tokenValue) {
-    if (player.identityToken) {
-      console.log('There is a conflict with an existing token');
-      process.exit(1);
-    }
-
-    player.setIdentityToken(tokenValue);
-    await dataAdapter.cleanup();
-
-    console.log('Token restored');
+if (tokenValue) {
+  if (player.identityToken) {
+    console.log('There is a conflict with an existing token');
+    process.exit(1);
   }
-  else {
-    if (player.identityToken) {
-      console.log('Using an existing identity token.');
-      console.log(`  To restore: npm run script bin/createIdentityToken.js ${playerId} ${player.identityToken}`);
-      console.log('');
-    }
-    else {
-      player.setIdentityToken();
-      await dataAdapter.cleanup();
-    }
 
-    console.log(`/addDevice.html?${player.identityToken.value}`);
+  player.setIdentityToken(tokenValue);
+
+  console.log('Token restored');
+} else {
+  if (player.identityToken) {
+    console.log('Using an existing identity token.');
+    console.log(`  To restore: npm run script bin/createIdentityToken.js ${playerId} ${player.identityToken}`);
+    console.log('');
+  } else {
+    player.setIdentityToken();
   }
-})();
+
+  console.log(`/addDevice.html?${player.identityToken.value}`);
+}
+
+await dataAdapter.cleanup();
