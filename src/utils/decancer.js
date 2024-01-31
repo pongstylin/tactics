8fb0b69919ff0421d0b8074b9e3dbccae9d7ab70
Dave Clark <tactics@dollardns.net>
2024-01-31 09:01:25 -0500

    Enhance name protection to handle I looking like l

--- src/utils/decancer.js
+++ src/utils/decancer.js
@@ -1,3 +1,7 @@
 import decancer from 'decancer';
 
-export default str => typeof str === 'string' ? decancer(str).toString().replace(/ /g, '') : str;
+export default str => typeof str === 'string'
+  ? decancer(str).toString()
+      .replace(/ /g, '')
+      .replace(/i/g, 'l')
+  : str;
