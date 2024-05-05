a23b4502439dec579b4413ae84451fa30d67efd4
unknown <tactics@dollardns.net>
2024-05-05 16:39:33 -0400

    Improve change obscene names script

--- bin/changeObsceneNames.js
+++ bin/changeObsceneNames.js
@@ -1,18 +1,20 @@
 import '#plugins/index.js';
 import AuthAdapter from '#data/FileAdapter/AuthAdapter.js';
-import { RegExpMatcher, englishDataset, englishRecommendedTransformers } from 'obscenity';
+import obscenity from '#utils/obscenity.js';
 
 const authAdapter = await new AuthAdapter().bootstrap();
 const playerIds = await authAdapter.listAllPlayerIds();
-const matcher = new RegExpMatcher({
-	...englishDataset.build(),
-	...englishRecommendedTransformers,
-});
 
 for (const playerId of playerIds) {
   const player = await authAdapter._getPlayer(playerId);
-  if (matcher.hasMatch(player.name))
-    player.updateProfile({ name:'Noob' });
+  if (obscenity.hasMatch(player.name)) {
+    for (let i = 1; i < 100; i++) {
+      try {
+        player.updateProfile({ name:i === 1 ? 'Noob' : `Noob${i}` });
+        break;
+      } catch (e) {}
+    }
+  }
 }
 
 await authAdapter.cleanup();
