d4d03aed2193dd111c0c2d6336993f9492fed11f
unknown <tactics@dollardns.net>
2024-05-05 16:23:23 -0400

    Improve obscenity filtering

--- src/utils/obscenity.js
+++ src/utils/obscenity.js
@@ -11,4 +11,10 @@ obscenityConfig.blacklistedTerms[98].pattern.requireWordBoundaryAtStart = false;
 // Fix Buttplug detection
 obscenityConfig.blacklistedTerms[108].pattern.nodes[0].chars = 'butplug'.split('').map(c => c.charCodeAt(0));
 
-export default new RegExpMatcher(obscenityConfig);
+const matcher = new RegExpMatcher(obscenityConfig);
+
+export default {
+  hasMatch: text => {
+    return matcher.hasMatch(text) || matcher.hasMatch(text.replace(/l/g, 'I'));
+  },
+};
