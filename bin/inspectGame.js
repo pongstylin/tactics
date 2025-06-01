333ad69d2b6e7cdd87fd73cbf3f055553c11d8a3
Dave Clark <tactics@dollardns.net>
2025-06-01 18:15:17 -0400

    Fix challenges appearing in public lists

--- bin/inspectGame.js
+++ bin/inspectGame.js
@@ -3,9 +3,10 @@ import '#plugins/index.js';
 import GameAdapter from '#data/DynamoDBAdapter/GameAdapter.js';
 
 const gameId = process.argv[2];
-const path = process.argv[3];
+const path = process.argv[3] ?? '$';
 
 const gameAdapter = await new GameAdapter({ hasState:false, readonly:true }).bootstrap();
 const game = await gameAdapter.getGame(gameId);
 
 console.log(jp(game, path));
+gameAdapter.cleanup();
