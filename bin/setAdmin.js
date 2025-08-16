6f2f8411c4b110a6a8728de6599a4e90bc951e33
Dave Clark <tactics@dollardns.net>
2025-08-16 08:06:36 -0400

    Slight improvement to setAdmin script

--- bin/setAdmin.js
+++ bin/setAdmin.js
@@ -2,11 +2,15 @@ import '#plugins/index.js';
 import AuthAdapter from '#data/DynamoDBAdapter/AuthAdapter.js';
 
 const playerId = process.argv[2];
-const dataAdapter = await new AuthAdapter({ useState:false }).bootstrap();
+const dataAdapter = await new AuthAdapter({ hasState:false }).bootstrap();
 const player = await dataAdapter.getPlayer(playerId);
+const wasAdmin = player.identity.admin;
 
 player.identity.admin = true;
 
 await dataAdapter.cleanup();
 
-console.log(`Player ${playerId} is now an admin. `);
+if (wasAdmin)
+  console.log(`Player ${playerId} was already an admin. `);
+else
+  console.log(`Player ${playerId} is now an admin. `);
