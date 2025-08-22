ad472c4756edd8c9b414412f5c7ef9e67d607d29
Dave Clark <tactics@dollardns.net>
2025-08-21 20:12:53 -0400

    Fix offline apps

--- src/ww.js
+++ src/ww.js
@@ -1,6 +1,7 @@
 /* Web Worker */
 
 import 'plugins/index.js';
+import Team from '#models/Team.js';
 import GameState from 'tactics/GameState.js';
 import serializer from 'utils/serializer.js';
 
@@ -23,21 +24,36 @@ self.addEventListener('message', ({ data:message }) => {
 
   if (type === 'create') {
     self.data = data;
-    self.state = GameState.create(data.clone())
+
+    const { teams, ...stateData } = data.clone();
+    stateData.numTeams = teams.length;
+
+    self.state = GameState.create(stateData)
       .on('sync', event => post('sync', event));
 
     post('init', self.state.getData());
 
-    if (self.state.teams.findIndex(t => !t?.joinedAt) === -1)
-      self.state.start();
+    for (const [ slot, teamData ] of teams.entries())
+      self.state.join(Team.create(Object.assign({}, teamData, {
+        slot,
+        joinedAt: new Date(),
+      })));
+    self.state.start();
   } else if (type === 'restart') {
-    self.state = GameState.create(self.data.clone())
+    const { teams, ...stateData } = self.data.clone();
+    stateData.numTeams = teams.length;
+
+    self.state = GameState.create(stateData)
       .on('sync', event => post('sync', event));
 
     post('init', self.state.getData());
 
-    if (self.state.teams.findIndex(t => !t?.joinedAt) === -1)
-      self.state.start();
+    for (const [ slot, teamData ] of teams.entries())
+      self.state.join(Team.create(Object.assign({}, teamData, {
+        slot,
+        joinedAt: new Date(),
+      })));
+    self.state.start();
   } else if (type === 'call') {
     const value = self.state[data.method](...data.args);
     // The method in the reply is only useful for debugging.
