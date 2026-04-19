da1e310a3a3c72a18818104fe51f54a695eb89aa
Dave Clark <tactics@dollardns.net>
2026-04-19 17:30:47 -0400

    Moved announcements to back end for easier release

--- src/online.js
+++ src/online.js
@@ -16,6 +16,7 @@ import ViewSetModal from 'components/Modal/ViewSet.js';
 import { getParam, unsetParam } from 'utils/hashParams.js';
 import sleep from 'utils/sleep.js';
 
+const api = Tactics.api;
 const authClient = Tactics.authClient;
 const gameClient = Tactics.gameClient;
 const pushClient = Tactics.pushClient;
@@ -288,61 +289,23 @@ const loadAvatarsAndDependants = Tactics.load(['avatars']).then(async () => {
 });
 
 const spinner = new Spinner({ autoShow:false });
-const announcements = [
-  {
-    id: 1,
-    startAt: new Date('2026-04-01T00:00:00Z'),
-    endAt: new Date('2026-04-15T00:00:00Z'),
-    message: `
-      <HEADER style="margin:0 8px; font-size:1.2em; font-weight:bold">STORM THE ARENA!⚡</HEADER>
-      <DIV style="margin:0 8px">
-        A $200 Prize Pool is waiting, and we've leveled the playing field! Whether you're a pro or a rising star, everyone has a shot at the gold:<BR>
-        <UL style="list-style:none; margin-left:16px; padding-left:0">
-          <LI style="margin:4px 0"><B>TOP TIER</B>: 3 Prizes for the ultimate champions.</LI>
-          <LI style="margin:4px 0"><B>CHALLENGER TIER</B>: 3 Prizes reserved for the "Bottom Bracket"—so even if you aren't a pro, you can still win CASH!</LI>
-          <LI style="margin:4px 0"><B>EXCLUSIVE REWARD</B>: Play all your games to claim the legendary Storm Dragon unit—no victory required!</LI>
-        </UL>
-        Don't just watch the storm. Join it.<BR>
-        Tap the arena below to join our Discord for sign-ups and full details!<BR>
-      </DIV>
-      <DIV style="text-align:center; margin-top:16px">
-        <A href="https://discord.gg/nuXcg65" target="_blank" style="display:inline-block; position:relative">
-          <IMG src="/StormDragonPreview.png" />
-          <SPAN style="display:block; position:absolute; bottom:32px; width:100%">Claim your reward!</SPAN>
-        </A>
-      </DIV>
-    `,
-  },
-  {
-    id: 2,
-    startAt: new Date('2026-04-15T00:00:00Z'),
-    endAt: new Date('2026-05-02T00:00:00Z'),
-    message: `
-      <HEADER style="margin:0 8px; font-size:1.2em; font-weight:bold">STORM THE ARENA!⚡</HEADER>
-      <DIV style="margin:0 8px">
-        Final call and reminder!  Don't miss out on your chance to earn $$ while playing in a brand new Tournament starting May 2nd.  If you play all of your games, you will earn the brand new Storm Dragon unit.  Tap the arena below to visit our Discord to learn more and sign up.
-      </DIV>
-      <DIV style="text-align:center; margin-top:16px">
-        <A href="https://discord.gg/nuXcg65" target="_blank" style="display:inline-block; position:relative">
-          <IMG src="/StormDragonPreview.png" />
-          <SPAN style="display:block; position:absolute; bottom:32px; width:100%">Claim your reward!</SPAN>
-        </A>
-      </DIV>
-    `,
-  },
-];
 
-whenDOMReady.then(async () => {
+api.get('/announcements').then(async announcements => {
   const now = Date.now();
   const responses = new Map(config.getItem('announcementResponses', []));
 
-  for (const announcement of announcements) {
-    // Disable for now
-    return;
+  await whenDOMReady;
+
+  for (const responseId of responses.keys())
+    if (!announcements.has(responseId))
+      responses.delete(responseId);
+
+  for (const [ announcementId, announcement ] of announcements) {
+    if (announcement.silent) continue;
     if (now < announcement.startAt) continue;
     if (now > announcement.endAt) continue;
 
-    const response = responses.get(announcement.id);
+    const response = responses.get(announcementId);
     if (response?.type === 'dismiss') continue;
     if (response?.type === 'remind' && Date.now() < new Date(response.startAt)) continue;
 
@@ -356,9 +319,9 @@ whenDOMReady.then(async () => {
     }).whenClosed;
 
     if (newResponse === 'remind')
-      responses.set(announcement.id, { type:'remind', startAt:new Date(Date.now() + 86400000) });
+      responses.set(announcementId, { type:'remind', startAt:new Date(Date.now() + 86400000) });
     else if (newResponse === 'dismiss')
-      responses.set(announcement.id, { type:'dismiss' });
+      responses.set(announcementId, { type:'dismiss' });
   }
 
   config.setItem('announcementResponses', responses);
--- src/server.js
+++ src/server.js
@@ -14,6 +14,7 @@ import { onConnect, onShutdown } from '#server/router.js';
 import services, { servicesReady } from '#server/services.js';
 import Timeout from '#server/Timeout.js';
 import useAuth from '#server/useAuth.js';
+import serializer from '#utils/serializer.js';
 
 const app    = createApp();
 const server = createServer(app);
@@ -54,6 +55,49 @@ app.post(`${PATH}/report`, (req, res) => {
   report(util.inspect(req.body, false, null));
   res.send(true);
 });
+app.get(`${PATH}/announcements`, (req, res) => res.send(serializer.transform(new Map([
+  [ 1, {
+    silent: true,
+    startAt: new Date('2026-04-01T00:00:00Z'),
+    endAt: new Date('2026-04-15T00:00:00Z'),
+    message: `
+      <HEADER style="margin:0 8px; font-size:1.2em; font-weight:bold">STORM THE ARENA!⚡</HEADER>
+      <DIV style="margin:0 8px">
+        A $200 Prize Pool is waiting, and we've leveled the playing field! Whether you're a pro or a rising star, everyone has a shot at the gold:<BR>
+        <UL style="list-style:none; margin-left:16px; padding-left:0">
+          <LI style="margin:4px 0"><B>TOP TIER</B>: 3 Prizes for the ultimate champions.</LI>
+          <LI style="margin:4px 0"><B>CHALLENGER TIER</B>: 3 Prizes reserved for the "Bottom Bracket"—so even if you aren't a pro, you can still win CASH!</LI>
+          <LI style="margin:4px 0"><B>EXCLUSIVE REWARD</B>: Play all your games to claim the legendary Storm Dragon unit—no victory required!</LI>
+        </UL>
+        Don't just watch the storm. Join it.<BR>
+        Tap the arena below to join our Discord for sign-ups and full details!<BR>
+      </DIV>
+      <DIV style="text-align:center; margin-top:16px">
+        <A href="https://discord.gg/nuXcg65" target="_blank" style="display:inline-block; position:relative">
+          <IMG src="/StormDragonPreview.png" />
+          <SPAN style="display:block; position:absolute; bottom:32px; width:100%">Tap to join!</SPAN>
+        </A>
+      </DIV>
+    `,
+  } ],
+  [ 2, {
+    silent: true,
+    startAt: new Date('2026-04-15T00:00:00Z'),
+    endAt: new Date('2026-05-02T00:00:00Z'),
+    message: `
+      <HEADER style="margin:0 8px; font-size:1.2em; font-weight:bold">STORM THE ARENA!⚡</HEADER>
+      <DIV style="margin:0 8px">
+        Final call and reminder!  Don't miss out on your chance to earn $$ while playing in a brand new Tournament starting May 2nd.  If you play all of your games, you will earn the brand new Storm Dragon unit.  Tap the arena below to visit our Discord to learn more and sign up.
+      </DIV>
+      <DIV style="text-align:center; margin-top:16px">
+        <A href="https://discord.gg/nuXcg65" target="_blank" style="display:inline-block; position:relative">
+          <IMG src="/StormDragonPreview.png" />
+          <SPAN style="display:block; position:absolute; bottom:32px; width:100%">Tap to join!</SPAN>
+        </A>
+      </DIV>
+    `,
+  } ],
+]))));
 app.post(`${PATH}/promote`, async (req, res) => {
   const tokenValue = req.headers.authorization?.replace(/^Bearer /, '');
   if (!tokenValue)
