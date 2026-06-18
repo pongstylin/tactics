24d7a734cc003bf4e3a61efa878307a964d3904c
Dave Clark <tactics@dollardns.net>
2026-06-17 22:03:14 -0400

    Restore static method since it is required for offline apps

--- src/tactics/GameType.js
+++ src/tactics/GameType.js
@@ -95,6 +95,21 @@ export default class GameType {
     this._tagByKeyword = null;
   }
 
+  static applyTeamSetUnitsState(unitsState, degree = 0) {
+    const board = new Board();
+
+    for (const unitState of unitsState) {
+      const unitData = unitDataMap.get(unitState.type);
+      const tile = board.getTileRotation(unitState.assignment, degree);
+
+      unitState.assignment = [tile.x, tile.y];
+      unitState.direction = unitData.directional === false ? 'S' : board.getRotation(unitState.direction ?? 'S', degree);
+      if (unitData.waitFirstTurn && !unitState.mRecovery)
+        unitState.mRecovery = 1;
+    }
+
+    return unitsState;
+  }
   static applyInitialGameState(team, skipTurn = true) {
     // First team must skip their first turn.
     if (skipTurn)
